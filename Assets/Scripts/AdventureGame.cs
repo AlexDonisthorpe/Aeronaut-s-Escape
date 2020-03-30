7a02592ff6ba54c349928d122c95a2860fd31a32
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-03-30 16:39:41 +0100

    Initialised Repo & Added .gitignore

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    // Start is called before the first frame update
+
+    State currentState;
+
+    void Start()
+    {
+        currentState = startingState;
+        textComponent.text = currentState.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = currentState.GetNextStates();
+    /*    if (Input.GetKeyDown(KeyCode.Alpha1)||Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            currentState = nextStates[0];
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)||Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            if (nextStates.Length > 1)
+            {
+                currentState = nextStates[1];
+            }
+        } else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            if (nextStates.Length > 2)
+            {
+                currentState = nextStates[2];
+            }
+        }
+    */
+
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index) || Input.GetKeyDown(KeyCode.Keypad1 + index))
+            {
+                currentState = nextStates[index];
+            }
+        }
+
+
+        textComponent.text = currentState.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+
+    [TextArea(10, 14)][SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory() => storyText;
+    public State[] GetNextStates() => nextStates;
+}
+
